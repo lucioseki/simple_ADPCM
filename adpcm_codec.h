fd63a7d83703dc95d8aed4eaa95fd2533a18bd56
Lucio Mitsuru Seki <lucioseki@gmail.com>
2013-01-31 23:28:28 -0200

    We will use a linked list of structs segmentHeader. Each header will store the informations of the segment, a pointer to the vector of values, and a pointer to the next header.

--- adpcm_codec.h
+++ adpcm_codec.h
@@ -1,5 +1,15 @@
 #include "wave_header.h"
 
-void adpcm_code(HeaderType *outHeader, uint16_t **outdata, const HeaderType inHeader, uint16_t **indata);
+#define MAXDIFF 2047
 
-void adpcm_decode(HeaderType *outHeader, uint16_t **outdata, const HeaderType inHeader, uint16_t **indata);
+typedef struct segmentHeader{
+	uint16_t keyValue;
+	uint16_t numSamples;
+	uint16_t segmentBPS; // bits per sample
+	uint16_t *segment;
+	segmentHeader *nextHeader;
+} segmentHeader;
+
+void adpcm_code(segmentHeader **outdata, uint16_t **outdata, const HeaderType inHeader, uint16_t **indata);
+
+void adpcm_decode(HeaderType *outHeader, uint16_t **outdata, const HeaderType inHeader, segmentHeader **indata);
